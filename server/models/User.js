e641f725c15573add2dbd2907577af9b2f7a33c1
Chris.Franklin <cfrank306@gmail.com>
2021-12-09 11:54:33 -0600

    added boilerplate to user

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
     _id: {
@@ -29,5 +30,20 @@ const userSchema = new Schema({
         type: [TripParks]
     }
 });
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 module.exports = userSchema;
