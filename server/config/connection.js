f26603c75a68519c7bb1b660505b254bb744a730
Chris.Franklin <cfrank306@gmail.com>
2021-12-09 10:59:56 -0600

    connection.js and auth.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/parksforrec', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/parksForRec', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
