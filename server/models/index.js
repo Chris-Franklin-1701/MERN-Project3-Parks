7cb3930f6d5193e484cf03e630aa70d717682a69
Alex Cwik <alexcwik1490@gmail.com>
2021-12-09 10:50:19 -0600

    added pages file and login.js

--- server/models/index.js
+++
@@ -1,6 +0,0 @@
-const User = require('./User');
-const Product = require('./Product');
-const Category = require('./Parks');
-const Order = require('./Order');
-
-module.exports = { User, Product, Category, Order };
