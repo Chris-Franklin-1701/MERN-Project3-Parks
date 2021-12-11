2b03d6bffaae893390f6b5008f94c132d95965ce
Alex Cwik <alexcwik1490@gmail.com>
2021-12-11 13:44:58 -0600

    changed font

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -22,7 +22,7 @@ const LandingPage = () => {
   return (
       <header>
         <div className="intro-logo jumbo-bg">
-          <h1 style={{textAlign: "center", alignItems: "center", display: "flex", flexDirection: "column", position: "absolute", fontSize: '2.5em', fontWeight: "700", fontFamily: "Kailasa, sans-serif", top: "5em", bottom: "0", left: "0", right: "0",}}>Plan Your Trip to Our Nations Many National Parks. <div>Click the Compass to Begin your Adventure!</div> </h1>
+          <h1 style={{ color: 'white', textAlign: "center", alignItems: "center", display: "flex", flexDirection: "column", position: "absolute", fontSize: '2.5em', fontWeight: "700", fontFamily: "Kailasa, sans-serif", top: "5em", bottom: "0", left: "0", right: "0",}}>Plan Your Trip to Our Nations Many National Parks. <div>Click the Compass to Begin your Adventure!</div></h1>
           <div className="text-center">
           <Link className="shadow-lg p-6 text-muted mb-5 rounded btn btn-lg m-4" style={{ opacity: '0.7', flexDirection: "column", position: "absolute",top: "20em", left: "59em",}} as={Link} to="/home"><img src= 'https://media.istockphoto.com/vectors/vintage-compass-vector-id142289062' style ={{maxHeight: '7em',}}></img></Link>
           <img src= 'https://www.stefanbaumann.com/wp-content/uploads/2014/07/Crater-Lake.jpg' style ={{ width: '160em'}}></img>
