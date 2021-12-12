d1cf7f49b6669dd28cf30a0fc0b6b02bf9f9a2a0
Alex Cwik <alexcwik1490@gmail.com>
2021-12-12 14:29:11 -0600

    better button, fixed footer buttons and formatted background.

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -20,14 +20,14 @@ const LandingPage = () => {
   if (referrer) return <Redirect to={referrer} />;
 
   return (
-      <header>
-        <div className="intro-logo jumbo-bg">
-          <h1 style={{textAlign: "center", alignItems: "center", display: "flex", flexDirection: "column", position: "absolute", fontSize: '2.5em', fontWeight: "700", fontFamily: "Kailasa, sans-serif", top: "5em", bottom: "0", left: "0", right: "0",}}>Plan Your Trip to Our Nations Many National Parks. <div>Click the Compass to Begin your Adventure!</div> </h1>
-          <div className="text-center">
-          <Link className="shadow-lg p-6 text-muted mb-5 rounded btn btn-lg m-4" style={{ opacity: '0.7', flexDirection: "column", position: "absolute",top: "20em", left: "59em",}} as={Link} to="/home"><img src= 'https://media.istockphoto.com/vectors/vintage-compass-vector-id142289062' style ={{maxHeight: '7em',}}></img></Link>
-          <img src= 'https://www.stefanbaumann.com/wp-content/uploads/2014/07/Crater-Lake.jpg' style ={{ width: '160em'}}></img>
-          </div>
-          </div>
+    <header>
+    <div className="intro-logo jumbo-bg">
+      <h1 style={{textAlign: "center", alignItems: "center", display: "flex", flexDirection: "column", position: "absolute", fontSize: '2.5em', fontWeight: "700", fontFamily: "Kailasa, sans-serif", top: "5em", bottom: "0", left: "0", right: "0",}}>Plan Your Trip to Our Nations Many National Parks. <div>Click the Compass to Begin your Adventure!</div> </h1>
+      <div className="text-center">
+      <Link className="btn btn-lg m-4" style={{flexDirection: "column", position: "absolute",top: "8em", left: "25em",}} as={Link} to="/home"><img src= 'https://i.imgur.com/89SLPRE.png' style ={{maxHeight: '40em',}}></img></Link>
+      <img src= 'https://www.stefanbaumann.com/wp-content/uploads/2014/07/Crater-Lake.jpg' style ={{ width: '100%', height: "100%", backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundSize: "cover",}}></img>
+      </div>
+      </div>
       </header>
     )
   };
