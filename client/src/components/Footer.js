d6c8f0e41aa09060a6ded5ae96abf319b51daab4
Chris.Franklin <cfrank306@gmail.com>
2021-12-13 21:43:22 -0600

    footer and white space fixed

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,19 +4,15 @@ import "../index.css";
 
 function Footer() {
 return (
-    <div className="footer">
     <footer className="bg-dark fixed-bottom">
         <div className="container">
-        <p className="m-0 text-center text-white">
-        <Link className="btn btn-lg btn-link m=2 text-light"to="/portfolio"><img src = 'https://i.imgur.com/jBk4CCe.png'></img></Link>
-        <Link className="btn btn-lg btn-link m=2 text-light" to="/contact"><img src = 'https://i.imgur.com/X5PRHtt.png'></img></Link>
-        <a className="btn btn-lg btn-link m=2 text-light" href="https://github.com/Chris-Franklin-1701/MERN-Project3-Parks"><img src = 'https://i.imgur.com/Ze9kryd.png'></img></a>
-        </p>
+            <p className="m-0 text-center text-white">
+            <Link className="btn btn-lg btn-link m=2 text-light"to="/portfolio"><img className="footer-img" src = 'https://i.imgur.com/jBk4CCe.png'></img></Link>
+            <Link className="btn btn-lg btn-link m=2 text-light" to="/contact"><img className="footer-img" src = 'https://i.imgur.com/X5PRHtt.png'></img></Link>
+            <a className="btn btn-lg btn-link m=2 text-light" href="https://github.com/Chris-Franklin-1701/MERN-Project3-Parks"><img className="footer-img" src = 'https://i.imgur.com/Ze9kryd.png'></img></a>
+            </p>
         </div>
-        
     </footer>
-    </div>
-    
 );
 }
 
--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../index.css";
 
 const LandingPage = () => {
   
   return (
     <header>
         <div className="overlay" />
+              <div className="intro-logo jumbo-bg">
+                  { 
+                  <h1 style={{color: 'white', textAlign: "center", alignItems: "center", display: "inline-flex", flexDirection: "column", position: "absolute", fontSize: '4em', fontWeight: "700", fontFamily: "avanta garde", top: "5em", bottom: "0", left: "0", right: "0", filter: 'drop-shadow(3px 2px 2px black', textDecoration: ''}}>
+                    Parks for Rec</h1> }
+              </div>
         <div className="outter-circle" />
         <div className="inner-circle">
           <Link to="/home">
-            <img
+            <img className="home-btn"
               src="https://www.worldatlas.com/r/w768/upload/54/6c/e9/compass-rose-daniel-tadevosyan.jpg"
               style={{
                 width: "100%",
@@ -22,12 +28,6 @@ const LandingPage = () => {
           </Link>
         </div>
       <body>  
-      <div className="intro-logo jumbo-bg">
-          { 
-          <h1 style={{color: 'white', textAlign: "center", alignItems: "center", display: "inline-flex", flexDirection: "column", position: "absolute", fontSize: '4em', fontWeight: "700", fontFamily: "avanta garde", top: "5em", bottom: "0", left: "0", right: "0", filter: 'drop-shadow(3px 2px 2px black', textDecoration: ''}}>
-            Parks for Rec <div></div> 
-            <div></div> </h1> }
-            </div>
       </body>
     </header>
   );
