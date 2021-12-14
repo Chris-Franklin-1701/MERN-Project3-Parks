698fb9a27fca2951a2b0d6a3ad0c2a53bd4ece7b
Chris.Franklin <cfrank306@gmail.com>
2021-12-13 23:41:49 -0600

    fixed landing page and navbar

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "../index.css";
 
@@ -6,59 +7,14 @@ const LandingPage = () => {
   return (
       <>
       <div className="overlay"></div>
-        <header
-        style={{display: 'flex', 
-        flexDirection: 'column', 
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '90vh',
-        overflow: 'hidden'
-        
-        }}>
-          
-
-          {/* <img
-            src="https://www.worldatlas.com/r/w768/upload/54/6c/e9/compass-rose-daniel-tadevosyan.jpg"
-            style={{
-              width: "100px",
-              height: "100px",
-              position: 'relative',
-              zIndex: '1',
-            }}
-          /> */}
+        <header id="landing-header">
         
         <div className="parksText">
-
+          <Nav.Link as={Link} to="/home">
             <img
-            src="https://i.imgur.com/ACg9toi.png" className="logo-animation"
-            style={{
-              width: "500px",
-              height: "auto",
-              position: 'relative',
-              zIndex: '1',
-              
-
-            }}
-          />
-            <h1
-              style={{
-                color: '#d6c088',
-                textAlign: "center",
-                alignItems: "center",
-                fontSize: "4em",
-                fontWeight: "700",
-                fontFamily: "avanta garde",
-                marginTop: "1em",
-                marginBottom: "0",
-                marginLeft: 'auto',
-                marginRight: 'auto',
-                justifySelf: "center",
-                filter: "drop-shadow(3px 2px 2px black",
-                textDecoration: "",
-    
-              }}
-            >
-              
+            src="https://i.imgur.com/ACg9toi.png" className="logo-animation"/>
+          </Nav.Link>
+            <h1>
               Parks for Rec 
             </h1>
           
